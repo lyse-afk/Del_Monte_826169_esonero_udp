f3ce9c96d71f4e99782dfab9aed1e4c4f5e652bd
lyse-afk <francescadelm10@gmail.com>
2025-12-08 15:35:36 +0100

    Update main.c

--- client-project/src/main.c
+++ client-project/src/main.c
@@ -1,5 +1,5 @@
 /*
- * client.c
+ * main.c
  *
  * UDP Client - Meteo
  */
@@ -14,13 +14,14 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #define closesocket close
 #endif
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <ctype.h>
+#include <string.h>
 #include "protocol.h"
 
 void clearwinsock() {
@@ -38,10 +39,10 @@ int main(int argc, char *argv[]) {
     int port = DEFAULT_PORT;
     const char *req_str = NULL;
 
-    for (int i=1; i<argc; ++i) {
-        if (strcmp(argv[i],"-s")==0 && i+1<argc) server_name = argv[++i];
-        else if (strcmp(argv[i],"-p")==0 && i+1<argc) port = atoi(argv[++i]);
-        else if (strcmp(argv[i],"-r")==0 && i+1<argc) req_str = argv[++i];
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-s") == 0 && i+1 < argc) server_name = argv[++i];
+        else if (strcmp(argv[i], "-p") == 0 && i+1 < argc) port = atoi(argv[++i]);
+        else if (strcmp(argv[i], "-r") == 0 && i+1 < argc) req_str = argv[++i];
         else { print_usage(argv[0]); return 1; }
     }
 
@@ -49,72 +50,76 @@ int main(int argc, char *argv[]) {
 
 #if defined WIN32
     WSADATA wsa_data;
-    if (WSAStartup(MAKEWORD(2,2), &wsa_data)!=0) { fprintf(stderr,"WSAStartup fallita\n"); return 1; }
+    if (WSAStartup(MAKEWORD(2,2), &wsa_data) != 0) { fprintf(stderr,"WSAStartup fallita\n"); return 1; }
 #endif
 
+    // Parsing richiesta
     if (req_str[0]=='\0' || req_str[1]!=' ') { fprintf(stderr,"Errore tipo richiesta\n"); return 1; }
-
     weather_request_t request;
+    memset(&request,0,sizeof(request));
     request.type = req_str[0];
 
     const char *city_start = req_str + 1;
     while (*city_start==' ') city_start++;
-
     if (strlen(city_start) >= CITY_MAX_LEN) { fprintf(stderr,"Nome città troppo lungo\n"); return 1; }
-
-    // Copia città e termina stringa
-    memset(request.city,0,sizeof(request.city));
     strncpy(request.city, city_start, CITY_MAX_LEN-1);
 
+    // Risoluzione server
     struct addrinfo hints, *res;
-    memset(&hints,0,sizeof(hints));
+    memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
 
-    if (getaddrinfo(server_name,NULL,&hints,&res)!=0) { perror("getaddrinfo"); return 1; }
+    if (getaddrinfo(server_name, NULL, &hints, &res) != 0) { perror("getaddrinfo"); return 1; }
     struct sockaddr_in server_addr = *(struct sockaddr_in*)res->ai_addr;
     server_addr.sin_port = htons((unsigned short)port);
     freeaddrinfo(res);
 
+    // Creazione socket UDP
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock<0) { perror("socket"); clearwinsock(); return 1; }
+    if (sock < 0) { perror("socket"); clearwinsock(); return 1; }
 
-    char buffer[1+CITY_MAX_LEN];
-    memset(buffer,0,sizeof(buffer));
+    // Serializzazione request
+    char buffer[1 + CITY_MAX_LEN];
     buffer[0] = request.type;
     memcpy(buffer+1, request.city, CITY_MAX_LEN);
 
+    // Invio datagram
     if (sendto(sock, buffer, sizeof(buffer), 0, (struct sockaddr*)&server_addr, sizeof(server_addr)) != sizeof(buffer)) {
-        perror("sendto"); closesocket(sock); clearwinsock(); return 1;
+        perror("sendto");
+        closesocket(sock); clearwinsock(); return 1;
     }
 
+    // Ricezione risposta
     char rbuffer[sizeof(uint32_t)+1+sizeof(uint32_t)];
     struct sockaddr_in from_addr;
     socklen_t from_len = sizeof(from_addr);
     int bytes = recvfrom(sock, rbuffer, sizeof(rbuffer), 0, (struct sockaddr*)&from_addr, &from_len);
     if (bytes != sizeof(rbuffer)) { fprintf(stderr,"Risposta invalida\n"); closesocket(sock); clearwinsock(); return 1; }
 
+    // Deserializzazione
     weather_response_t response;
     uint32_t tmp;
 
-    memcpy(&tmp,rbuffer,sizeof(uint32_t));
+    memcpy(&tmp, rbuffer, sizeof(uint32_t));
     response.status = ntohl(tmp);
     response.type = rbuffer[4];
-    memcpy(&tmp,rbuffer+5,sizeof(uint32_t));
+    memcpy(&tmp, rbuffer+5, sizeof(uint32_t));
     tmp = ntohl(tmp);
-    memcpy(&response.value,&tmp,sizeof(float));
+    memcpy(&response.value, &tmp, sizeof(float));
 
-    if (response.status==STATUS_OK) {
-        switch(response.type) {
+    // Stampa risultato
+    if (response.status == STATUS_OK) {
+        switch (response.type) {
             case TYPE_TEMP:  printf("Temperatura = %.1f°C\n", response.value); break;
             case TYPE_HUM:   printf("Umidità = %.1f%%\n", response.value); break;
             case TYPE_WIND:  printf("Vento = %.1f km/h\n", response.value); break;
             case TYPE_PRESS: printf("Pressione = %.1f hPa\n", response.value); break;
             default: printf("Risposta non valida\n"); break;
         }
-    } else if (response.status==STATUS_CITY_NOT_FOUND) printf("Città non disponibile\n");
-    else if (response.status==STATUS_BAD_REQUEST) printf("Richiesta non valida\n");
-    else printf("Risposta non valida\n");
+    } else {
+        printf("Richiesta non valida\n");
+    }
 
     closesocket(sock);
     clearwinsock();
