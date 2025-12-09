451af5a335d02b9e74caa4b8440560441b5fd599
lyse-afk <francescadelm10@gmail.com>
2025-12-09 10:12:30 +0100

    Refactor protocol.h by cleaning up definitions
    Removed unused definitions and constants, updated structures.

--- client-project/src/protocol.h
+++ client-project/src/protocol.h
@@ -2,39 +2,35 @@
  * protocol.h
  *
  * Client header file
+ * Definitions, constants and structures for client/server communication
  */
+
 #ifndef PROTOCOL_H_
 #define PROTOCOL_H_
 
-#define BUFFER_SIZE 512
-#define QUEUE_SIZE  5
+// Shared application parameters
+#define SERVER_PORT 56700
+#define STATUS_OK 0
+#define STATUS_CITY_NOT_FOUND 1
+#define STATUS_INVALID_REQUEST 2
 
-#define DEFAULT_PORT 56700
-#define CITY_MAX_LEN 64
-
-#define STATUS_OK             0u
-#define STATUS_CITY_NOT_FOUND 1u
-#define STATUS_BAD_REQUEST    2u
-
-#define TYPE_TEMP  't'
-#define TYPE_HUM   'h'
-#define TYPE_WIND  'w'
-#define TYPE_PRESS 'p'
-
+// Message Client to Server
 typedef struct {
-    char type;                   // Weather data type: 't', 'h', 'w', 'p'
-    char city[CITY_MAX_LEN];     // City name (null-terminated string)
+    char type;        // Weather data type: 't', 'h', 'w', 'p'
+    char city[64];    // City name (null-terminated string)
 } weather_request_t;
 
+// Message Server to Client
 typedef struct {
-    unsigned int status;         // 0,1,2
-    char type;                   // eco del type, o '\0' se errore
-    float value;                 // dato meteo
+    unsigned int status;  // Response status code
+    char type;            // Echo of request type
+    float value;          // Weather data value
 } weather_response_t;
 
-float get_temperature(void);
-float get_humidity(void);
-float get_wind(void);
-float get_pressure(void);
+// Server data generation functions
+float get_temperature(void);    // Range: -10.0 to 40.0 °C
+float get_humidity(void);       // Range: 20.0 to 100.0 %
+float get_wind(void);           // Range: 0.0 to 100.0 km/h
+float get_pressure(void);       // Range: 950.0 to 1050.0 hPa
 
 #endif /* PROTOCOL_H_ */
